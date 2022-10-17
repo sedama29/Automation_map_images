3381e6d2a4e978d402663bc3830a447562d32d2d
Sathwika Edama <108834058+sedama29@users.noreply.github.com>
2022-10-18 03:21:14 +0530

    Add files via upload

--- org.py
+++ org.py
@@ -1,20 +1,20 @@
 from selenium import webdriver 
-import os
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 chromeOptions = webdriver.ChromeOptions()
 prefs = {'safebrowsing.enabled': 'false',
-'download.default_directory' : 'D:\map_images\plat_new'}
+'download.default_directory' : 'D:\map_images_2\Organizations'}
 chromeOptions.add_experimental_option("prefs", prefs)
 chromeOptions.add_experimental_option("excludeSwitches", ["enable-logging"])
 driverService = Service('D:/GRIIDC_Automation-main/GRIIDC_Automation-main/chromedriver.exe') 
 driver = webdriver.Chrome(service=driverService,options=chromeOptions)
-for i in range(1,60):
+for i in range(40,41):
     y = str(i)
     driver.get('http://localhost/map_images/mapCP_NTL2.php?org='+y+'&make=0')
-        # driver.implicitly_wait(5000)
     time.sleep(2)
     btn_ok = driver.find_element(By.ID, "save-image-btn").click()
-time.sleep(3000)
+    time.sleep(2)
+time.sleep(30)
 driver.close()
--- plat.py
+++ plat.py
@@ -1,19 +1,19 @@
 from selenium import webdriver
-import os
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 chromeOptions = webdriver.ChromeOptions()
 import sqlite3
 connection = sqlite3.connect("D:\database_new\gcoos_data_v3.sqlite")
 crsr = connection.cursor()
 prefs = {'safebrowsing.enabled': 'false',
-'download.default_directory' : 'D:\map_images\Platforms'}
+'download.default_directory' : 'D:\map_images_2\Platforms'}
 chromeOptions.add_experimental_option("prefs", prefs)
 chromeOptions.add_experimental_option("excludeSwitches", ["enable-logging"])
 driverService = Service('D:/GRIIDC_Automation-main/GRIIDC_Automation-main/chromedriver.exe') 
 driver = webdriver.Chrome(service=driverService,options=chromeOptions)
-for i in range(1,60):
+for i in range(45,62):
     y = str(i)
     crsr.execute('''select p.rowid from platform p join organization o on p.orgId =  o.rowId where o.rowId =?;''', (i,))
     ans = crsr.fetchall()
@@ -21,9 +21,9 @@ for i in range(1,60):
     for j in ans:
         x = str(j[0])
         driver.get('http://localhost/map_images/mapCP_NTL2.php?org='+y+'&plat='+x+'&make=1')
-        # driver.implicitly_wait(5000)
         time.sleep(2)
         btn_ok = driver.find_element(By.ID, "save-image-btn").click()
-time.sleep(3000)
+        time.sleep(1)
+time.sleep(30)
 connection.close()
 driver.close()
